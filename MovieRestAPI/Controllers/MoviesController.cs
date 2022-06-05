6c526f1a5c9e1b9f3f6ecfaefd347fc871eab7ad
Tobias <tobi195h@edu.easj.dk>
2022-06-06 01:08:40 +0200

    Add project files.

---
+++ MovieRestAPI/Controllers/MoviesController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MovieLib;
+using MovieRestAPI.Managers;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MovieRestAPI.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase{
+        private MoviesManager manager = new MoviesManager();
+        // GET: api/<MoviesController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> Get() {
+            return Ok(manager.GetAllMovies());
+        }
+
+        // GET api/<MoviesController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Movie> Get(int id) {
+            return Ok(manager.GetMovieById(id));
+        }
+
+        // POST api/<MoviesController>
+        [HttpPost]
+        public ActionResult<Movie> Post([FromBody] Movie newMovie){
+            return manager.PostMovie(newMovie);
+        }
+
+        // PUT api/<MoviesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) {
+        }
+
+        // DELETE api/<MoviesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id) {
+        }
+    }
+}
---
+++ MovieRestAPI/Managers/MoviesManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using MovieLib;
+
+namespace MovieRestAPI.Managers {
+    public class MoviesManager{
+        private static int nextId = 1;
+
+            private static List<Movie> moviesList = new List<Movie>(){
+                new Movie(){Country = "Denmark", Id = nextId++, LengthInMinutes = 120, MovieName = "Druk"},
+                new Movie(){Country = "Korea", Id = nextId++, LengthInMinutes = 174, MovieName = "Train to Busan"},
+                new Movie(){Country = "America", Id = nextId++, LengthInMinutes = 143, MovieName = "Star Wars"},
+            };
+
+        public IEnumerable<Movie> GetAllMovies(){
+            IEnumerable<Movie> result = moviesList;
+            return result;
+        }
+
+        public Movie GetMovieById(int id){
+            var result = moviesList.Find(m => m.Id == id);
+            return result;
+        }
+
+        public Movie PostMovie(Movie newMovie){
+            newMovie.Id = nextId++;
+            moviesList.Add(newMovie);
+            return newMovie;
+        }
+    }
+}
