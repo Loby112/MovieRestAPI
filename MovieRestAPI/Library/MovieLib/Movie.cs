36bce9c5c302c7d40c9753442864589a27ec51cb
Loby112 <90177951+Loby112@users.noreply.github.com>
2022-06-06 13:35:05 +0200

    Add files via upload

---
+++ MovieRestAPI/Library/MovieLib/Movie.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MovieLib {
+    public class Movie {
+        public int Id{ get; set; }
+        public string MovieName{ get; set; }
+        public int LengthInMinutes{ get; set; }
+        public string Country{ get; set; }
+    }
+}
---
+++ MovieRestAPI/Library/MovieLib/TimeCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLib {
+    public static class TimeCalculator { 
+        public static int ConvertMinutesToSeconds(int minutes){
+            if (minutes <= 0){
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return minutes * 60;
+        }
+    }
+}
